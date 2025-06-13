dda0a182a0c28cd5b04b9e91c2095916b4bc8bcf
Ram Deshmukh <ramdeshmukh3003@gmail.com>
2025-06-13 19:15:10 +0530

    commited

--- configs/db-config.js
+++ configs/db-config.js
@@ -5,7 +5,7 @@ const {DB_URL} = process.env;
 const dbConnection = () => {
     mongoose.connect(DB_URL)
         .then(() => console.log('Database Connection Successfull'))
-        .catch(err => console.log('Failed To Connect With Database, \nReason : ' + err.message))
+        .catch(err => console.log('Failed while Connecting With Database, \nReason : ' + err.message))
 }
 
 module.exports = dbConnection;
